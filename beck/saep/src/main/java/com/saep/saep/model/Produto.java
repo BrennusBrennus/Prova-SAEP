fe29003fceb289d5fb11ed4aee8f48001bc7b4a2
nxtannnn1 <natanwill.contatar@outlook.com>
2025-11-26 14:37:31 -0300

    add usuario

---
+++ beck/saep/src/main/java/com/saep/saep/model/Produto.java
@@ -0,0 +1,4 @@
+package com.saep.saep.model;
+
+public class Produto {
+}
---
+++ beck/saep/src/main/java/com/saep/saep/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.saep.saep.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "usuario_id")
+    private UUID id;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
